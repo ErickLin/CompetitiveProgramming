d1e7cdec47f5d15c9dcbe4145431276393038a03
ErickLin <erickscott1957@gmail.com>
2015-07-27 22:39:37 -0400

    correct solution for Musical Themes

--- USACO/theme.cpp
+++ USACO/theme.cpp
@@ -22,50 +22,25 @@ int main(int argc, char** argv) {
         return 0;
     }
     vector<int> notes(n);
-    vector<int> d(n - 1);
     for (int i = 0; i < n; i++) {
         fin >> notes[i];
-        if (i > 0) {
-            d[i - 1] = notes[i] - notes[i - 1];
-        }
     }
-    int maxLen = -1;
-    //theme must be at least five notes long
-    int lo = 4, hi = (d.size() >> 1);
-    //do binary search over lengths of sequences of pitch changes
-    while (lo <= hi) {
-        int mid = lo + ((hi - lo) >> 1);
-        //maps each theme to the indices of its first and last occurrences
-        map<vector<int>, pair<int, int> > themeIndices;
-        for (int i = 0; i <= d.size() - mid && maxLen < mid; i++) {
-            vector<int> theme(mid);
-            for (int j = 0; j < mid; j++) {
-                theme[j] = d[i + j];
-            }
-            if (themeIndices.find(theme) == themeIndices.end()) {
-                themeIndices[theme] = make_pair(i, i);
+    int maxLen = 0;
+    for (int i = 1; i < n; i++) {
+        int len = 1;
+        for (int j = 0; j < n - i; j++) {
+            if ((notes[j + 1] - notes[j]) == (notes[i + j + 2] - notes[i + j + 1])) {
+                if (len <= i) {
+                    len++;
+                }
+                if (len > maxLen) {
+                    maxLen = len;
+                }
             } else {
-                pair<int, int> indices = themeIndices[theme];
-                if (i < indices.first) {
-                    themeIndices[theme].first = i;
-                }
-                if (i > indices.second) {
-                    themeIndices[theme].second = i;
-                }
-                indices = themeIndices[theme];
-                //check that subsequences of notes, not differences
-                //(hence the extra 1), are disjoint
-                if (indices.second - indices.first - 1 >= mid) {
-                    maxLen = mid;
-                }
+                len = 1;
             }
         }
-        if (maxLen == mid) {
-            lo = mid + 1;
-        } else {
-            hi = mid - 1;
-        }
     }
-    fout << maxLen + 1 << '\n';
+    fout << (maxLen < 5 ? 0 : maxLen) << '\n';
     return 0;
 }
