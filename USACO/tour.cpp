de1ce6b48d481f104c02d536c5a758a9e1ff5b41
ErickLin <erickscott1957@gmail.com>
2015-08-04 13:05:16 -0400

    correct solution for Canada Tour

--- USACO/tour.cpp
+++ USACO/tour.cpp
@@ -11,36 +11,6 @@ TASK: tour
 #include <vector>
 using namespace std;
 
-int dfs(int v, vector<set<int> > &edges, vector<set<int> > &edgesBack
-        , vector<bool> &vis, int numVis, bool returning) {
-    int n = edges.size();
-    if (v == 0 && returning) {
-        //counted first city twice
-        return numVis - 1;
-    } else if (v == n - 1 || returning) {
-        int ret = 1;
-        for (set<int>::iterator it = edgesBack[v].begin(); it != edgesBack[v].end(); it++) {
-            if (!vis[*it]) {
-                vis[*it] = true;
-                ret = max(ret, dfs(*it, edges, edgesBack, vis, numVis + 1, true));
-                vis[*it] = false;
-            }
-        }
-        return ret;
-    } else if (!returning) {
-        int ret = 1;
-        for (set<int>::iterator it = edges[v].begin(); it != edges[v].end(); it++) {
-            if (!vis[*it]) {
-                vis[*it] = true;
-                ret = max(ret, dfs(*it, edges, edgesBack, vis, numVis + 1, false));
-                vis[*it] = false;
-            }
-        }
-        return ret;
-    }
-    return 1;
-}
-
 int main() {
     ifstream fin("tour.in");
     ofstream fout("tour.out");
@@ -53,18 +23,45 @@ int main() {
         indices[city] = i;
     }
     vector<set<int> > edges(n);
-    vector<set<int> > edgesBack(n);
     for (int i = 0; i < m; i++) {
         string a, b;
         fin >> a >> b;
-        if (indices[a] < indices[b]) {
-            edges[indices[a]].insert(indices[b]);
-            edgesBack[indices[b]].insert(indices[a]);
-        } else {
-            edges[indices[b]].insert(indices[a]);
-            edgesBack[indices[a]].insert(indices[b]);
+        edges[indices[a]].insert(indices[b]);
+        edges[indices[b]].insert(indices[a]);
+    }
+    //represents the max number of cities that can be visited starting at first index,
+    //visiting the first city, and ending at second index
+    vector<vector<int> > dp(n, vector<int>(n));
+    //base case: at most one flight from i to 0 and at most one flight from 0 to j
+    dp[0][0] = 1;
+    for (int i = 1; i < n; i++) {
+        if (edges[i].find(0) != edges[i].end()) {
+            dp[i][0] = dp[0][i] = 2;
+            for (int j = 1; j < i; j++) {
+                if (edges[j].find(0) != edges[j].end()) {
+                    dp[i][j] = dp[j][i] = 3;
+                }
+            }
         }
     }
-    vector<bool> visited(n);
-    fout << dfs(0, edges, edgesBack, visited, 1, false) << '\n';
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            if (i != j && dp[i][j] > 0) {
+                for (int k = min(1 + max(i, j), n - 1); k < n; k++) {
+                    if (edges[i].find(k) != edges[i].end() && dp[k][j] < dp[i][j] + 1) {
+                        dp[k][j] = dp[i][j] + 1;
+                    }
+                    if (edges[j].find(k) != edges[j].end() && dp[i][k] < dp[i][j] + 1) {
+                        dp[i][k] = dp[i][j] + 1;
+                    }
+                }
+            }
+        }
+    }
+    if (dp[n - 1][n - 1] > 0) {
+        fout << dp[n - 1][n - 1] - 1 << '\n';
+    } else {
+        fout << "1\n";
+    }
+    return 0;
 }
