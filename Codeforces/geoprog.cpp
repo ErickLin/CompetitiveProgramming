ba3ce10c0ba179f15cf50d03302d012aebb7a6df
ErickLin <erickscott1957@gmail.com>
2015-08-05 15:18:53 -0400

    Codeforces Round #Pi - A, B, C

---
+++ Codeforces/geoprog.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main() {
+    map<int, int> count;
+    map<int, long long> count2;
+    int n, k;
+    cin >> n >> k;
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if ((long long) a % ((long long) k * k) == 0) {
+            ans += count2[a / k];
+        }
+        if (a % k == 0) {
+            count2[a] += count[a / k];
+        }
+        count[a]++;
+    }
+    cout << ans << '\n';
+}
---
+++ Codeforces/lineland.cpp
@@ -0,0 +1,32 @@
+#include <climits>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    vector<int> minDist(n);
+    fill(minDist.begin(), minDist.end(), INT_MAX);
+    vector<int> maxDist(n);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+        if (i > 0) {
+            minDist[i] = x[i] - x[i - 1];
+            maxDist[i] = x[i] - x[0];
+            if (x[i] - x[i - 1] < minDist[i - 1]) {
+                minDist[i - 1] = x[i] - x[i - 1];
+            }
+        }
+    }
+    for (int i = 0; i < n - 1; i++) {
+        if (x[n - 1] - x[i] > maxDist[i]) {
+            maxDist[i] = x[n - 1] - x[i];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << minDist[i] << ' ' << maxDist[i] << '\n';
+    }
+    return 0;
+}
