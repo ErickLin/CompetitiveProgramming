e0022b1282992d553a601bfc758076a238a63c48
ErickLin <erickscott1957@gmail.com>
2015-08-24 23:40:35 -0400

    Codeforces Round #317 (Div. 2) - A, B

---
+++ Codeforces/arrays.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+//Codeforces Round #317 - Div. 2A
+
+int main() {
+    int na, nb, k, m;
+    cin >> na >> nb >> k >> m;
+    vector<int> a(na), b(nb);
+    for (int i = 0; i < na; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < nb; i++) {
+        cin >> b[i];
+    }
+    if (a[k - 1] < b[nb - m]) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
---
+++ Codeforces/orderbook.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+using namespace std;
+//Codeforces Round #317 - Div. 2B
+
+struct cmp {
+    bool operator()(const int &a, const int &b) {
+        return a > b;
+    }
+};
+
+int main() {
+    int n, s;
+    cin >> n >> s; 
+    map<int, int, cmp> buy;
+    map<int, int> sell;
+    for (int i = 0; i < n; i++) {
+        char d;
+        int p, v;
+        cin >> d >> p >> v;
+        if (d == 'B') {
+            buy[p] += v;
+        } else {
+            sell[p] += v;
+        }
+    }
+    int i = 0;
+    stack<pair<int, int> > stk;
+    for (map<int, int>::iterator it = sell.begin(); i < s && it != sell.end(); it++) {
+        stk.push(make_pair(it->first, it->second));
+        i++;
+    }
+    while (!stk.empty()) {
+        cout << 'S' << ' ' << stk.top().first << ' ' << stk.top().second << '\n';
+        stk.pop();
+    }
+    i = 0;
+    for (map<int, int>::iterator it = buy.begin(); i < s && it != buy.end(); it++) {
+        cout << 'B' << ' ' << it->first << ' ' << it->second << '\n';
+        i++;
+    }
+    return 0;
+}
