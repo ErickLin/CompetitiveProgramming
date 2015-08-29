a58d11cf995123d08b0d2d4a71dddc01c4d276d8
ErickLin <erickscott1957@gmail.com>
2015-08-29 15:06:02 -0400

    Codeforces Round #318 (Div. 1) - A, B

---
+++ Codeforces/blocks.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+//Codeforces Round #318 - Div. 1B
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> h(n);
+	for (int i = 0; i < n; i++) {
+		cin >> h[i];
+	}
+	h[0] = 1;
+	h[n - 1] = 1;
+	for (int i = 1; i < n; i++) {
+		if (h[i] > h[i - 1] + 1) {
+			h[i] = h[i - 1] + 1;
+		}
+	}
+	for (int i = n - 2; i >= 0; i--) {
+		if (h[i] > h[i + 1] + 1) {
+			h[i] = h[i + 1] + 1;
+		}
+	}
+	int maxHeight = 1;
+	for (int i = 0; i < n; i++) {
+		if (h[i] > maxHeight) {
+			maxHeight = h[i];
+		}
+	}
+	cout << maxHeight << '\n';
+}
---
+++ Codeforces/poker.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+//Codeforces Round #318 - Div. 1A
+
+long long gcd(long long a, long long b) {
+	if (max(a, b) % (min(a, b)) == 0) {
+		return min(a, b);
+	} else {
+		return gcd(min(a, b), max(a, b) % min(a, b));
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	int gcdTot = gcd(a[0], a[1]);
+	for (int i = 2; i < n; i++) {
+		gcdTot = gcd(gcdTot, a[i]);
+	}
+	bool possible = true;
+	for (int i = 0; i < n && possible; i++) {
+		a[i] /= gcdTot;
+		int temp = a[i];
+		while (temp % 2 == 0 || temp % 3 == 0) {
+			if (temp % 2 == 0) {
+				temp /= 2;
+			} else {
+				temp /= 3;
+			}
+		}
+		if (temp != 1) {
+			possible = false;
+		}
+	}
+	if (possible) {
+		cout << "Yes\n";
+	} else {
+		cout << "No\n";
+	}
+    return 0;
+}
